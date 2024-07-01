6f1f395e1a4520ea84c71cc38640935a86fb2c37
devlara <createdevlara@gmail.com>
2024-07-01 12:58:11 +0900

    vue설치 및 기본활용/데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--안녕하세요. -->
+<div>
+  <h4 class="red" style="스타일">{{ products[0]}}</h4>
+  <p>{{ price1 }} 만원</p>
+</div>
+<div>
+  <h4>{{ products[1] }}</h4>
+  <p>{{ price2 }} 만원</p>
+</div>
+<div>
+  <h4>{{ products[2]}}</h4>
+  <p>{{ price3 }} 만원</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// document.getElementById().innerHTML = ??  //원래 쌩 자바스크립트 형식 입력, vue에선 아래 추가하면서 간단히 만들어서 데이터 보관 할 수 있다. 
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price1 : 50,      //위와 맞춰준다! , 자료이름: 자료내용
+      price2 : 60,
+      price3 : 40,
+      // 또는 prices: [50,60,40] 형식으로 한번에 배열로 저장하고, 위도 idex맞춰줄 수 있다!
+
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
+  
   }
 }
 </script>
